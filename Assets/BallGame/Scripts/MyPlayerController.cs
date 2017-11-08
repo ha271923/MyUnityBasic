7e300ba7e77246f57a9b8c01795cd7f96ba3e6b6
ha271923 <ha271923@gmail.com>
2017-11-08 17:09:54 +0800

    Add a plane

--- Assets/BallGame/Scripts/MyPlayerController.cs
+++ Assets/BallGame/Scripts/MyPlayerController.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyPlayerController : MonoBehaviour {
 
     private Rigidbody rb;
     public float speed;
+	private int count;
+	public Text countText;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
-		
+
+		// float moveHorizontal = Input.GetAxis("Mouse X");
+		// float moveVertical = Input.GetAxis("Mouse Y");
+
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		
 		rb.AddForce(movement);
@@ -26,9 +34,14 @@ public class MyPlayerController : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Pick Up Tag")) {
 			other.gameObject.SetActive(false);
+			count += 1;
+			setCountText();
 		}
 	}
 
+	void setCountText() {
+		countText.text = "Count: "+ count.ToString();
+	}
 
 
 }
